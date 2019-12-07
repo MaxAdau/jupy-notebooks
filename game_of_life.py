0e6633bd4eb54027348079d4c378bf208d0144ae
Max <maxadau@gmail.com>
2019-12-07 14:35:40 +0100

    Óptimizing perf : redraw rectangle instead of creating new ones at each loop ....

--- game_of_life.py
+++ game_of_life.py
@@ -1,40 +1,30 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ## My game of life
-# 
-# Ok, it's been a while since I wanted to implement a game fo life, but lack of time and motivation were against me.
-# I have both right now so ... let's do this a pythonic way !
-# 
-# ### What is the game of life ?
-# 
-# It is a cellular automation, designed by Conway in 1970
-# More info here : https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
-# 
-# ### Rules of the game :
-# - A cell can be either dead or alive
-# - Any live cell with two or three neighbors survives
-# - Any dead cell with three live neighbors becomes a live cell
-# - All other live cells die in the next generation. Similarly, all other dead cells stay dead
-
 import tkinter as tk
 
+from time import time, sleep
+prev = time()
+
+def delta(txt):
+    global prev
+    print(txt)
+    print('deltatime = {}'.format(time() - prev))
+    prev = time()
 
 class Grid:
     """
     This class implement the grid that contains all the cells in the game
     """
+
     def __init__(self, canvas, width, height, cell_size):
         self.cell_size = cell_size
-        self.canvas = canvas    
+        self.canvas = canvas
 
         # Variable calculated from default ones
         self.nbr_cell_width = int(width / cell_size)
         self.nbr_cell_height = int(height / cell_size)
-        
+
         # List of all cells in the grid
         self.cells = []
-        
+
     def generate_grid(self):
         """
         Generate aa grid with empty rectangle
@@ -45,14 +35,15 @@ class Grid:
             y1 = grid_y * self.cell_size
             y2 = grid_y * self.cell_size + self.cell_size
             self.cells.append([])
+            # Loop in X
             for grid_x in range(0, self.nbr_cell_width):
                 x1 = grid_x * self.cell_size
                 x2 = grid_x * self.cell_size + self.cell_size
-                # Append the cell in the double list array
-                self.cells[grid_y].append(Cell(grid_x, grid_y,
-                                               x1, y1, x2, y2,
-                                               self.canvas))
-    
+
+                # Create the rectangle using canvas and store the rectangle id in the cell
+                rect_id = self.canvas.create_rectangle(x1, y1, x2, y2, fill='white')
+                self.cells[grid_y].append(Cell(grid_x, grid_y, self.canvas, rect_id))
+
     def compute_next_grid(self):
         """
         Iterate over all cells and set their next_gen attribute
@@ -64,7 +55,7 @@ class Grid:
                 nbr_neighbors = self._get_alive_neighbors(cell)
                 # print('Cell {} has {} neighbors'. format(cell.get_xy(), nbr_neighbors))
                 self.cells[y][x].next_gen = self._apply_rules(cell, nbr_neighbors)
-    
+
     def swap_status(self, tkevent):
         """
         Change the status of a cell
@@ -76,7 +67,7 @@ class Grid:
         # Change the current status of the cell and draw it
         self.cells[y][x].is_alive = not self.cells[y][x].is_alive
         self.cells[y][x].draw_cell()
-    
+
     def draw_next_gen(self):
         """
         Update the cell.isActivate attribute
@@ -86,14 +77,14 @@ class Grid:
             for x, cell in enumerate(row):
                 self.cells[y][x].is_alive = self.cells[y][x].next_gen
                 self.cells[y][x].draw_cell()
-                
+
     def _apply_rules(self, cell, nbr_neighbors):
         """
         Calculate the next value of a specific cell, either
         return True if the cell will be alive
         return False if not
         """
-        
+
         # Rules of the game
         # Any live cell with two or three neighbors survives
         if cell.is_alive and nbr_neighbors in [2, 3]:
@@ -111,16 +102,16 @@ class Grid:
         """
         # Coordinates to apply to current cell to get all 8 neighbors
         neighbors = [(-1, -1), (0, -1), (1, -1),
-                     (-1,  0),          (1,  0),
-                     (-1,  1), (0,  1), (1,  1)]
-        
+                     (-1, 0), (1, 0),
+                     (-1, 1), (0, 1), (1, 1)]
+
         # Iterate over neighbors and get the is_alive status
         nbr_neighbors = 0
         for coordinates in neighbors:
             adjusted_x = cell.grid_x + coordinates[0]
             adjusted_y = cell.grid_y + coordinates[1]
             # print('Cell {} : looking for cell {}'.format(cell.get_xy(),(adjusted_x, adjusted_y)))
-            
+
             # Try to get the neighbors
             if adjusted_x >= 0 and adjusted_y >= 0:
                 try:
@@ -140,9 +131,9 @@ class Grid:
         Translate a x and y tkinter coordinates in a grid xy position
         Trick : I use int() to round down my coordinates
         """
-        return(int(x / self.cell_size),
-               int(y / self.cell_size))
-    
+        return (int(x / self.cell_size),
+                int(y / self.cell_size))
+
     def _debug(self):
         """
         Print all cells and their status
@@ -156,44 +147,36 @@ class Cell:
     """
     This class represent a cell as intended in the game of life
     """
-    def __init__(self, grid_x, grid_y, x1, y1, x2, y2, canvas):
-        
+
+    def __init__(self, grid_x, grid_y, canvas, rect_id):
+
         # Position of the cell in the grid
         self.grid_x = grid_x
         self.grid_y = grid_y
-        
-        # Position of the cell in the canvas
-        self.x1 = x1
-        self.x2 = x2
-        self.y1 = y1
-        self.y2 = y2
-        
+
         # The canvas used to perform graphical magic
         self.canvas = canvas
-        
+        self.rect_id = rect_id
+
         # Used for the game logic
         self.is_alive = False
         self.next_gen = False
-        
+
         # Automatically display the cell when instantiate
         self.draw_cell()
-        
+
     def draw_cell(self):
         """
         Draw the cell
         """
-        color = ''
-
         # Cell is alive
         if not self.is_alive:
             color = 'white'
         # Cell is dead
-        elif self.is_alive:
+        else:
             color = 'black'
-        # Draw the rectangle
-        self.canvas.create_rectangle(self.x1, self.y1,
-                                     self.x2, self.y2,
-                                     fill=color)
+        # Update the rectangle color
+        self.canvas.itemconfig(self.rect_id, fill=color)
 
     def get_xy(self):
         """
@@ -204,22 +187,23 @@ class Cell:
     def __str__(self):
         return '{} : {} --> {}'.format((self.grid_x, self.grid_y),
                                        self.is_alive, self.next_gen)
-         
+
 
 class Game:
     """
     This class contains the game logic
     """
+
     def __init__(self):
         # Game default values
         width = 600
         height = 600
         cell_size = 20
-        
+
         # Instantiation of the main windows
         self.root = tk.Tk()
         self.root.title("My game of life ! \0/")
-        
+
         # Instantiation of the frame on which the canvas will be added
         # pack() organizes widgets in blocks before placing them in the parent widget
         # Without it, the main windows will remain at default size
@@ -231,7 +215,7 @@ class Game:
         # The Canvas widget provides structured graphics facilities for Tkinter
         self.canvas = tk.Canvas(self.frame, width=width, height=height)
         self.canvas.pack()
-    
+
         # Place buttons and link functions to them
         start_button = tk.Button(self.root, text="Start game", command=self.start)
         start_button.pack(side=tk.LEFT)
@@ -241,17 +225,18 @@ class Game:
         # For debug purpose only
         # debug_button = tk.Button(self.root, text="Next loop", command=self.game_loop)
         # debug_button.pack(side = tk.BOTTOM)
+        self.idx = 0
 
         # Create the grid and generate the visible rectangles
         self.grid = Grid(self.canvas, width, height, cell_size)
         self.grid.generate_grid()
-        
+
         # link the left click action to the swap status function
         self.canvas.bind("<Button-1>", self.grid.swap_status)
 
         # Launch the main loop
         self.root.mainloop()
-        
+
     def start(self):
         """
         Start the game
@@ -259,14 +244,23 @@ class Game:
         # I don't want a petit rigolo to change the status of cells
         self.canvas.unbind("<Button-1>")
         self.game_loop()
-    
+
     def game_loop(self):
         """
         Contains the main loop of the game
         """
+        print('\n---------new_loop---------')
+        delta('New_loop')
         self.grid.compute_next_grid()
+        delta('After grid.compute_next_grid()')
         self.grid.draw_next_gen()
+        delta('After grid.draw_next_gen')
         self.root.after(200, self.game_loop)
+        delta('After root.after')
+
+        self.idx += 1
+        if self.idx == 100:
+            self.root.destroy()
 
     def stop(self):
         self.root.destroy()
@@ -274,3 +268,5 @@ class Game:
 
 if __name__ == '__main__':
     game = Game()
+
+
